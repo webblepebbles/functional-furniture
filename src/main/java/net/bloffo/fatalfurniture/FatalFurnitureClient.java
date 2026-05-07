be2580f759d638cef8a9b85b8f09a273fba56ca7
dylan <dylanbilbao62@gmail.com>
2026-05-07 15:11:53 +1000

    stuffs that i forgot to commit for months

--- src/main/java/net/bloffo/fatalfurniture/FatalFurnitureClient.java
+++ src/main/java/net/bloffo/fatalfurniture/FatalFurnitureClient.java
@@ -1,6 +1,8 @@
 package net.bloffo.fatalfurniture;
 
 import net.bloffo.fatalfurniture.block.ModBlocks;
+import net.bloffo.fatalfurniture.block.entity.ModBlockEntities;
+import net.bloffo.fatalfurniture.block.entity.renderer.TableBlockEntityRenderer;
 import net.bloffo.fatalfurniture.entity.ModEntities;
 import net.bloffo.fatalfurniture.entity.client.ChairRenderer;
 import net.bloffo.fatalfurniture.entity.client.CushionRenderer;
@@ -8,6 +10,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class    FatalFurnitureClient implements ClientModInitializer {
     @Override
@@ -15,5 +18,7 @@ public class    FatalFurnitureClient implements ClientModInitializer {
 
         EntityRendererRegistry.register(ModEntities.CHAIR, ChairRenderer::new);
         EntityRendererRegistry.register(ModEntities.CUSHION, CushionRenderer::new);
+
+        BlockEntityRendererFactories.register(ModBlockEntities.TABLE_BE, TableBlockEntityRenderer::new);
     }
 }
---
+++ src/main/java/net/bloffo/fatalfurniture/block/entity/renderer/TableBlockEntityRenderer.java
@@ -0,0 +1,44 @@
+package net.bloffo.fatalfurniture.block.entity.renderer;
+
+import net.bloffo.fatalfurniture.block.entity.custom.TableBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.world.LightType;
+import net.minecraft.world.World;
+
+public class TableBlockEntityRenderer implements BlockEntityRenderer<TableBlockEntity> {
+    public TableBlockEntityRenderer(BlockEntityRendererFactory.Context context){
+
+    }
+
+    @Override
+    public void render(TableBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
+        ItemStack stack = entity.getStack(0);
+
+        matrices.push();
+        matrices.translate(0.5f, 0.53125f, 0.5f);
+        matrices.scale(0.5f, 0.5625f, 0.5f);
+        matrices.multiply(RotationAxis.NEGATIVE_X.rotation(33.0f));
+
+        itemRenderer.renderItem(stack, ModelTransformationMode.GUI, getLightLevel(entity.getWorld(),
+                entity.getPos()), OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), 1);
+        matrices.pop();
+    }
+
+    private int getLightLevel(World world, BlockPos pos) {
+        int bLight = world.getLightLevel(LightType.BLOCK, pos);
+        int sLight = world.getLightLevel(LightType.SKY, pos);
+        return LightmapTextureManager.pack(bLight, sLight);
+    }
+}
